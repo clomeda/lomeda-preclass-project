c4973d70d5b793b71e669a5c056f0184c3a73e65
clomeda <clomeda01@gmail.com>
2023-02-22 15:12:18 +0800

    Legal age

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -1,10 +1,8 @@
 def legal(age)
   if age>18
-    puts "You are an adult"
-  elsif age<=18 && age!=1
-    puts "You are not adult"
-  else
-    puts "You're not even born yet."
+    puts "You're an adult.\n"
+  else age<=18 && age!=1
+    puts "You're not adult.\n"
   end
 end
-p legal(5)
+p legal(24)
